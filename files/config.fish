dc2fd52a8ec1994aeec005cdbbac764746575ba3
Juan Diego Romero González <jd@inkatze.com>
2017-12-15 14:43:19 -0600

    Added Postgres.app

--- files/config.fish
+++ files/config.fish
@@ -2,6 +2,7 @@
 set -xg GPG_TTY (tty)
 set -xg PYENV_ROOT $HOME/.pyenv
 set -xg PATH $PATH /usr/local/opt/python/libexec/bin
+set -xg PATH $PATH /Applications/Postgres.app/Contents/Versions/latest/bin
 
 function c
     z $argv
