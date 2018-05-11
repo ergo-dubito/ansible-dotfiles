c97daea3f45a57c6ecb0178435bc4cdd78491997
Juan Diego Romero González <jd@inkatze.com>
2018-05-11 13:35:45 -0700

    Shell fixes

--- files/config.fish
+++ files/config.fish
@@ -1,18 +1,20 @@
 # env vars
 set -xg fish_greeting '¡Hoal!'
-set -xg GPG_TTY (tty)
-set -xg PYENV_ROOT $HOME/.pyenv
-set -xg GOPATH $HOME/dev/go
-
-set -xg PATH /usr/local/opt/python/libexec/bin $PATH
-set -xg PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
-set -xg PATH $GOPATH/bin $PATH
-
 
 function c
     z $argv
 end
 
 if status --is-login
+    set -xg GPG_TTY (tty)
+    set -xg PYENV_ROOT $HOME/.pyenv
+    set -xg GOPATH $HOME/dev/go
+    set -xg PIPENV_SHELL_FANCY 1
+
+    set -xg PATH /usr/local/opt/python/libexec/bin $PATH
+    set -xg PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
+    set -xg PATH $GOPATH/bin $PATH
     set -xg PATH /usr/local/bin $PATH
 end
+
+source /usr/local/miniconda3/etc/fish/conf.d/conda.fish
