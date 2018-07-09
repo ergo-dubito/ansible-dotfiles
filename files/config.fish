deec2bdfa3a5151f9f6b115c7a7447e641a54773
Juan Diego Romero González <jd@inkatze.com>
2018-07-09 13:01:12 -0700

    Added default python version for pipenv

--- files/config.fish
+++ files/config.fish
@@ -9,6 +9,7 @@ if status --is-login
     set -xg PYENV_ROOT $HOME/.pyenv
     set -xg GOPATH $HOME/dev/go
     set -xg PIPENV_SHELL_FANCY 1
+    set -xg PIPENV_DEFAULT_PYTHON_VERSION 3.7
 
     set -xg PATH /usr/local/opt/python/libexec/bin $PATH
     set -xg PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
